7c5cd02c82f0c1224dc51063a274286c9f3f42f2
Gustavopf1 <gugapires300@gmail.com>
2026-09-13 10:17:58 -0300

    adiciona exercicio 06 de vetores e matrizes

---
+++ src/VetoresMatrizes/Exercicio06.java
@@ -0,0 +1,35 @@
+package VetoresMatrizes;
+
+import java.util.Scanner;
+
+public class Exercicio06 {
+    void main(){
+        Scanner sc = new Scanner(System.in);
+
+        int[] numeros = new int[6];
+        for (int i = 0; i < 6; i++) {
+            IO.print("Informe um número inteiro: ");
+            numeros[i] = sc.nextInt();
+        }
+        IO.print("Informe qual número voce deseja substituir: ");
+        int numeroX = sc.nextInt();
+
+        IO.print("Informe o novo valor: ");
+        int numeroY = sc.nextInt();
+
+        boolean encontrou = false;
+        for (int j = 0; j < 6; j++) {
+            if (numeros[j] == numeroX) {
+                numeros[j] = numeroY;
+                encontrou = true;
+            }
+        }
+        if (encontrou == false) {
+            IO.print("Número nao encontrado!");
+        }
+
+        for (int k = 0; k < 6; k++) {
+            IO.println(numeros[k]);
+        }
+    }
+}
